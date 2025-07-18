201df9ac6bbcb208fe0b62eccccd90a166d4781e
Arkadiusz Sieczak <sieczak.a@gmail.com>
2025-07-18 14:34:07 +0200

    fix: script enhancements

--- argocd/install.sh
+++ argocd/install.sh
@@ -2,4 +2,4 @@
 
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
-helm install argocd argo/argo-cd --namespace argocd --create-namespace --values values.yaml
+helm install argocd argo/argo-cd --namespace argocd --create-namespace --values values-initial-argo.yaml
