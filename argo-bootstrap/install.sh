4f954cf238eb6363609c532a9da09954a2fec605
Arkadiusz Sieczak <sieczak.a@gmail.com>
2025-08-01 10:17:09 +0200

    feat: argocd initial script

--- argo-bootstrap/install.sh
+++ argo-bootstrap/install.sh
@@ -3,3 +3,5 @@
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 helm install argocd argo/argo-cd --namespace argocd --create-namespace --values values-initial-argo.yaml
+
+# helm upgrade argocd -n argocd argo/argo-cd -f values-initial-argo.yaml
