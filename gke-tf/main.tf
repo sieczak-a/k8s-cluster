bddaf234ea4c6e4910df8218cdac82612e5524c0
Arkadiusz Sieczak <sieczak.a@gmail.com>
2025-08-03 23:26:27 +0200

    fix: gcp alerts

--- gke-tf/main.tf
+++ gke-tf/main.tf
@@ -5,7 +5,7 @@ resource "google_service_account" "gke_sa" {
 
 resource "google_project_iam_member" "gke_sa_roles" {
   for_each = toset([
-    "roles/container.nodeServiceAccount",
+    "roles/container.defaultNodeServiceAccount",
     "roles/monitoring.metricWriter",
     "roles/monitoring.viewer",
     "roles/logging.logWriter"
